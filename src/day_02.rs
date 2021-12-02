e6537b56cb868525759aa7986a842f65013cb635
Kyle Strohbeck <kstrohbeck@gmail.com>
2021-12-02 09:47:07 -0600

    Do day 2.

---
+++ src/day_02.rs
@@ -0,0 +1,82 @@
+use nom::IResult;
+
+pub fn star_1(data: String) {
+    let commands = parse(&data);
+    let mut horiz = 0;
+    let mut depth = 0;
+    for command in commands {
+        match command.direction {
+            Direction::Forward => horiz += command.distance,
+            Direction::Down => depth += command.distance,
+            Direction::Up => depth -= command.distance,
+        }
+    }
+    println!("{}", horiz * depth);
+}
+
+pub fn star_2(data: String) {
+    let commands = parse(&data);
+    let mut horiz = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for command in commands {
+        let dist = command.distance as i64;
+        match command.direction {
+            Direction::Forward => {
+                horiz += dist;
+                depth += aim * dist;
+            }
+            Direction::Down => aim += dist,
+            Direction::Up => aim -= dist,
+        }
+    }
+    println!("{}", horiz * depth);
+}
+
+fn parse(data: &str) -> Vec<Command> {
+    commands(data).unwrap().1
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Command {
+    direction: Direction,
+    distance: u64,
+}
+
+fn command(input: &str) -> IResult<&str, Command> {
+    use nom::{
+        character::complete::{space1, u64 as u64_},
+        combinator::map,
+        sequence::separated_pair,
+    };
+
+    map(separated_pair(direction, space1, u64_), |(dir, dis)| {
+        Command {
+            direction: dir,
+            distance: dis,
+        }
+    })(input)
+}
+
+fn commands(input: &str) -> IResult<&str, Vec<Command>> {
+    use nom::{character::complete::line_ending, multi::separated_list0};
+
+    separated_list0(line_ending, command)(input)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+fn direction(input: &str) -> IResult<&str, Direction> {
+    use nom::{branch::alt, bytes::complete::tag, combinator::value};
+
+    alt((
+        value(Direction::Forward, tag("forward")),
+        value(Direction::Down, tag("down")),
+        value(Direction::Up, tag("up")),
+    ))(input)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env, fs::File, io::Read};
 
 mod day_01;
-// mod day_02;
+mod day_02;
 // mod day_03;
 // mod day_04;
 // mod day_05;
@@ -40,8 +40,8 @@ fn main() {
     let func = match (day, star) {
         (1, 1) => day_01::star_1,
         (1, 2) => day_01::star_2,
-        // (2, 1) => day_02::star_1,
-        // (2, 2) => day_02::star_2,
+        (2, 1) => day_02::star_1,
+        (2, 2) => day_02::star_2,
         // (3, 1) => day_03::star_1,
         // (3, 2) => day_03::star_2,
         // (4, 1) => day_04::star_1,
