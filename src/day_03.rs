55571d2775ca9b510f1d881cacb23a59513ffe8c
Kyle Strohbeck <kstrohbeck@gmail.com>
2021-12-03 17:59:52 -0600

    Do day 3.

---
+++ src/day_03.rs
@@ -0,0 +1,177 @@
+use nom::IResult;
+use std::ops::Not;
+
+pub fn star_1(data: String) {
+    let nums = parse(&data);
+    let mut accum = vec![0i64; nums[0].len() as usize];
+    for num in nums {
+        for (i, bit) in num.bits().enumerate() {
+            accum[i] += match bit {
+                0 => -1,
+                _ => 1,
+            };
+        }
+    }
+    let gamma = accum
+        .iter()
+        .map(|b| if *b > 0 { 1 } else { 0 })
+        .fold(Number::default(), |acc, bit| acc.push_bit(bit));
+    let epsilon = !gamma;
+    let gamma: u64 = gamma.into();
+    let epsilon: u64 = epsilon.into();
+
+    println!("{}", gamma * epsilon);
+}
+
+pub fn star_2(data: String) {
+    let nums = parse(&data);
+
+    fn part(nums: &Vec<Number>, flip: bool) -> u64 {
+        let mut nums = nums.clone();
+        for i in 0..nums[0].len() {
+            let (zero, one): (Vec<Number>, _) = nums.iter().partition(|num| num.bit(i) == 0);
+            let pick_zero = zero.len() > one.len();
+            nums = if pick_zero == flip { one } else { zero };
+            if nums.len() <= 1 {
+                break;
+            }
+        }
+        nums[0].into()
+    }
+
+    let oxy = part(&nums, false);
+    let co2 = part(&nums, true);
+
+    println!("{}", oxy * co2);
+}
+
+fn parse(data: &str) -> Vec<Number> {
+    numbers(data).unwrap().1
+}
+
+fn numbers(input: &str) -> IResult<&str, Vec<Number>> {
+    use nom::{character::complete::line_ending, multi::separated_list0};
+
+    separated_list0(line_ending, number)(input)
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Number {
+    bits: u64,
+    len: u8,
+}
+
+impl Number {
+    pub fn len(self) -> u8 {
+        self.len
+    }
+
+    pub fn bit(self, bit: u8) -> u64 {
+        let offset = self.len - bit - 1;
+        (self.bits & (1 << offset)) >> offset
+    }
+
+    pub fn bits(self) -> impl Iterator<Item = u64> {
+        (0..self.len).map(move |bit| self.bit(bit))
+    }
+
+    fn mask(self) -> u64 {
+        (1 << self.len) - 1
+    }
+
+    pub fn push_bit(self, bit: u64) -> Self {
+        Self {
+            bits: (self.bits << 1) + bit,
+            len: self.len + 1,
+        }
+    }
+}
+
+impl Default for Number {
+    fn default() -> Self {
+        Self { bits: 0, len: 0 }
+    }
+}
+
+impl From<Number> for u64 {
+    fn from(number: Number) -> Self {
+        number.bits
+    }
+}
+
+impl Not for Number {
+    type Output = Self;
+
+    fn not(self) -> Self::Output {
+        Self {
+            bits: (!self.bits) & self.mask(),
+            len: self.len,
+        }
+    }
+}
+
+fn number(input: &str) -> IResult<&str, Number> {
+    use nom::multi::fold_many1;
+
+    fold_many1(bit, Number::default, |acc, bit| acc.push_bit(bit))(input)
+}
+
+fn bit(input: &str) -> IResult<&str, u64> {
+    use nom::{branch::alt, bytes::complete::tag, combinator::value};
+
+    alt((value(0, tag("0")), value(1, tag("1"))))(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn bits_are_correct() {
+        let num = Number {
+            bits: 0b1011100,
+            len: 7,
+        };
+        assert_eq!(vec![1, 0, 1, 1, 1, 0, 0], num.bits().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn mask_is_correct() {
+        let num = Number {
+            bits: 0b1011100,
+            len: 7,
+        };
+        assert_eq!(0b1111111, num.mask());
+    }
+
+    #[test]
+    fn not_flips_bits() {
+        let num = Number {
+            bits: 0b1011100,
+            len: 7,
+        };
+        assert_eq!(
+            Number {
+                bits: 0b0100011,
+                len: 7
+            },
+            !num
+        );
+    }
+
+    #[test]
+    fn number_is_parsed() {
+        let input = "1011100\n";
+        let num = number(input);
+        assert_eq!(
+            Ok((
+                "\n",
+                Number {
+                    bits: 0b1011100,
+                    len: 7
+                }
+            )),
+            num
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{env, fs::File, io::Read};
 
 mod day_01;
 mod day_02;
-// mod day_03;
+mod day_03;
 // mod day_04;
 // mod day_05;
 // mod day_06;
@@ -42,8 +42,8 @@ fn main() {
         (1, 2) => day_01::star_2,
         (2, 1) => day_02::star_1,
         (2, 2) => day_02::star_2,
-        // (3, 1) => day_03::star_1,
-        // (3, 2) => day_03::star_2,
+        (3, 1) => day_03::star_1,
+        (3, 2) => day_03::star_2,
         // (4, 1) => day_04::star_1,
         // (4, 2) => day_04::star_2,
         // (5, 1) => day_05::star_1,
