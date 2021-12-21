104287af4cacba3ca2a234315d3fe7e75ecc37e1
Kyle Strohbeck <kstrohbeck@gmail.com>
2021-12-21 17:50:31 -0600

    Simplify day 21.

--- src/day_21.rs
+++ src/day_21.rs
@@ -2,63 +2,75 @@ use nom::IResult;
 use std::collections::HashMap;
 
 pub fn star_1(data: String) {
-    let (mut p1, mut p2) = parse(&data);
-    let mut p1_score = 0;
-    let mut p2_score = 0;
-    let mut rolled = 0;
-    let mut dice = DetDice(1);
-    let diff = loop {
-        p1 = (p1 + dice.roll_three() - 1) % 10 + 1;
-        rolled += 3;
-        p1_score += p1;
-        if p1_score >= 1000 {
-            break p2_score * rolled;
+    let (p1, p2) = parse(&data);
+    let mut p1 = PlayerState::new(p1);
+    let mut p2 = PlayerState::new(p2);
+    let mut dice = DetDice::new();
+    let loser = loop {
+        if let Some(x) = step(&mut p1, &p2, &mut dice) {
+            break x;
         }
-        p2 = (p2 + dice.roll_three() - 1) % 10 + 1;
-        rolled += 3;
-        p2_score += p2;
-        if p2_score >= 1000 {
-            break p1_score * rolled;
+        if let Some(x) = step(&mut p2, &p1, &mut dice) {
+            break x;
         }
     };
-    println!("{}", diff);
+    let score = loser * dice.times_rolled;
+    println!("{}", score);
 }
 
 pub fn star_2(data: String) {
     let (mut p1, mut p2) = parse(&data);
-    let mut states = HashMap::new();
-    states.insert((PlayerState::new(p1), PlayerState::new(p2)), 1);
+    let mut p1_quantum_state = QuantumState::new(PlayerState::new(p1), PlayerState::new(p2));
+    let mut p2_quantum_state = QuantumState::empty();
     let mut p1_victories: u64 = 0;
     let mut p2_victories: u64 = 0;
-    let mut new_states = HashMap::new();
-    while !states.is_empty() {
-        // Do P1 move.
-        for ((p1, p2), num_univ) in states.drain() {
-            for (roll, branches) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
-                let p1 = p1.after_roll(roll);
-                let num_univ = num_univ * branches;
-                if p1.score >= 21 {
-                    p1_victories += num_univ;
-                } else {
-                    *new_states.entry((p1, p2)).or_insert(0) += num_univ;
-                }
-            }
-        }
-
-        // Do P2 move.
-        for ((p1, p2), num_univ) in new_states.drain() {
-            for (roll, branches) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
-                let p2 = p2.after_roll(roll);
-                let num_univ = num_univ * branches;
-                if p2.score >= 21 {
-                    p2_victories += num_univ;
-                } else {
-                    *states.entry((p1, p2)).or_insert(0) += num_univ;
-                }
-            }
-        }
+    while !p1_quantum_state.0.is_empty() {
+        p1_victories += p1_quantum_state.drain_step(&mut p2_quantum_state);
+        p2_victories += p2_quantum_state.drain_step(&mut p1_quantum_state);
     }
     println!("{}", p1_victories.max(p2_victories));
+}
+
+fn step(a: &mut PlayerState, b: &PlayerState, dice: &mut DetDice) -> Option<u64> {
+    let roll = dice.roll_three();
+    let new_a = a.after_roll(roll);
+    std::mem::replace(a, new_a);
+    if a.score >= 1000 {
+        Some(b.score)
+    } else {
+        None
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct QuantumState(HashMap<(PlayerState, PlayerState), u64>);
+
+impl QuantumState {
+    fn empty() -> Self {
+        Self(HashMap::new())
+    }
+
+    fn new(a: PlayerState, b: PlayerState) -> Self {
+        let mut state = HashMap::new();
+        state.insert((a, b), 1);
+        Self(state)
+    }
+
+    fn drain_step(&mut self, next: &mut Self) -> u64 {
+        let mut victories = 0;
+        for ((a, b), num_univ) in self.0.drain() {
+            for (roll, branches) in [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)] {
+                let a = a.after_roll(roll);
+                let num_univ = num_univ * branches;
+                if a.score >= 21 {
+                    victories += num_univ;
+                } else {
+                    *next.0.entry((b, a)).or_insert(0) += num_univ;
+                }
+            }
+        }
+        victories
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -82,12 +94,20 @@ impl PlayerState {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-struct DetDice(u64);
+struct DetDice {
+    state: u64,
+    times_rolled: u64,
+}
 
 impl DetDice {
+    fn new() -> Self {
+        Self::default()
+    }
+
     fn roll(&mut self) -> u64 {
-        let ret = self.0;
-        self.0 = self.0 % 100 + 1;
+        let ret = self.state;
+        self.state = self.state % 100 + 1;
+        self.times_rolled += 1;
         ret
     }
 
@@ -96,6 +116,15 @@ impl DetDice {
         let roll_2 = self.roll();
         let roll_3 = self.roll();
         roll_1 + roll_2 + roll_3
+    }
+}
+
+impl Default for DetDice {
+    fn default() -> Self {
+        Self {
+            state: 1,
+            times_rolled: 0,
+        }
     }
 }
 
